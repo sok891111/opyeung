4dd9ac0d107898f05e8fb862ab5e67476502183f
KimJaeHwan <relighting123@gmail.com>
2021-07-04 20:09:20 +0900

    Detail Page 수정 - 스크롤 시 메인 화면 다시 보이도록

--- client_react/src/Components/templates/Template.js
+++ client_react/src/Components/templates/Template.js
@@ -10,90 +10,73 @@ import ReactDOM from 'react-dom';
 class template extends Component {
   constructor(props) {
     super(props);
-    this.state ={
+    this.state = {
       Y: 0,
-      detailYN  : false
-    }
-}
-componentDidMount() {
-  // 스크롤링 이벤트 추가
-  window.addEventListener("scroll", this._onScroll);
-}
-
-
-_onScroll = (e) => {
-  console.log('스크롤 완료')
-  const scrollTop = ('scroll', e.srcElement.scrollingElement.scrollTop);
-  console.log(scrollTop)
-  this.setState({
-    detailYN : scrollTop>=this.state.Y,
-    Y : scrollTop
-  })
-  console.log(this.refs.current)
-}
-
-state = {
-  width:'100%',
-  height:'100%'
-}
-
-/*
-componentDidMount() {
-  fetch('http://34.83.107.247:8888/product/list')
-    .then(res => res.json())
-    .then(result => {
-      console.log('sfsf')
-      console.log(result)
-      this.setState({
-        isLoaded: true,
-        items: result
-      });
-    });
-}
-*/
-handleDrag=()=> {
-  console.log('드래그 완료')
-  this.setState({width:0,height:0})
- }
-  render() {
-    const box_active = {
-      
-      width:'100%',
-      height:'100%',
-      opacity: "1",
-      transition: "opacity 500ms",
-    };
-    
-    const box_hidden = {
-      width:'0%',
-      height:'0%',
-      opacity: "0",
-      visibility: "hidden",
-      transition: "opacity 500ms , visibility 500ms",
-    };
-    const detailPost_active = {
-      zindex:1,
-      position:'absolute',
-      top:'0px',
-      width:'100%',
-      height:'100%',
-      opacity: "1",
-      transition: "opacity 500ms",
-    };
-    return (
-     <div>
-         <Opheader />
-      <div   className = 'PostContainer' ref='PostContainer'>
-        
-        <Oppostlist Oppostlistdata = {this.props.Oppostlistdata} />
-      </div>
-      <div style={this.state.detailYN ? detailPost_active : {top:'1000px',width:'0px',height:'0px'}}  className = 'DetailPostContainer'  ref='DetailPostContainer'>
-       <OppostDetail  />
-          </div>
-    </div>
-    );
+      detailYN: false
     }
   }
+  componentDidMount() {
+    // 스크롤링 이벤트 추가
+    window.addEventListener("scroll", this._onScroll);
+  }
+
+
+  _onScroll = (e) => {
+    const scrollTop = ('scroll', e.srcElement.scrollingElement.scrollTop);
+    console.log(scrollTop)
+    this.setState({
+      detailYN: (scrollTop >= this.state.Y) || (scrollTop > 10),
+      Y: scrollTop
+    })
+    console.log(this.refs.current)
+  }
+
+  state = {
+    width: '100%',
+    height: '100%'
+  }
+
+  /*
+  componentDidMount() {
+    fetch('http://34.83.107.247:8888/product/list')
+      .then(res => res.json())
+      .then(result => {
+        console.log('sfsf')
+        console.log(result)
+        this.setState({
+          isLoaded: true,
+          items: result
+        });
+      });
+  }
+  */
+  handleDrag = () => {
+    console.log('드래그 완료')
+    this.setState({ width: 0, height: 0 })
+  }
+  render() {
+    const detailPost_active = {
+      zindex: 1,
+      position: 'absolute',
+      top: '0px',
+      width: '100%',
+      height: '100%',
+      opacity: "1",
+      transition: "opacity 5000ms",
+    };
+    return (
+      <div>
+        <Opheader />
+        <div className='PostContainer' ref='PostContainer'>
+          <Oppostlist Oppostlistdata={this.props.Oppostlistdata} />
+        </div>
+        <div style={this.state.detailYN ? detailPost_active : { top: '1000px', width: '0px', height: '0px' }} className='DetailPostContainer' ref='DetailPostContainer'>
+          <OppostDetail />
+        </div>
+      </div>
+    );
+  }
+}
 
 
 export default template;
